3954464d642026bc142569ccf231ae188bfe256c
Bernardo Kyotoku <bernardo@kyotoku.org>
2012-07-17 19:24:33 -0300

    remove print number of records

--- niScope.py
+++ niScope.py
@@ -437,7 +437,6 @@ no longer want to export and set outputTerminal to NISCOPE_VAL_NONE.
 				self.FetchNumberRecords = numberOfRecords/numberOfChannels
 			assert numAcquiredWaveforms == numberOfRecords
 			assert self.RecordLength >= samplesPerRecord
-		print samplesPerRecord
 			
 		data_type = {
 			numpy.float64 	:''	        ,
