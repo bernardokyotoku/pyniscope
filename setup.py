375d58a3ef0f1e7c288253cb35e755d2145a9d2b
John Vishnefske <jvishnefske@gmail.com>
2017-06-21 11:57:52 -0500

    prepare for pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,26 @@
 from setuptools import setup
-setup(name='niscope',
-      version='0.2',
-      py_modules=['niscope', 'niscope.niScopeTypes',
-                  'niscope.ordered_symbols'],
-      install_requires=['numpy']
-      )
+setup(name='pyniscope',
+      version='0.3',
+      packages=['niscope'],
+      install_requires=['numpy'],
+      author="Bernardo Kyotoku, John Vishnefske",
+      author_email="jvdude.developer@mailnull.com",
+      description="Python wrapper for NI Oscilloscopes",
+      url="https://github.com/bernardokyotoku/pyniscope",
+      long_description=open("README.md").read(),
+      classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: Microsoft :: Windows :: Windows 7",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 2.7"
+        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Intended Audience :: Science/Research"
+    ],
+
+    )
